3997a35391847229424c337ba68bfcd95eb67e16
bussiere <bussiere@gmail.com>
2016-11-05 23:00:08 +0100

    encore noettyage

--- login/v1/login.js
+++ login/v1/login.js
@@ -4,8 +4,12 @@
 
 
 function testLogin(req,res,callback) {
-    var wid     = req.params.wid;
-    res.status(200).json("OK");
+    var response = new Object();
+    response.status = 1;
+    response.message = "";
+    response.data = "OK";
+    var responseJson = JSON.stringify(response);
+    res.status(200).send(responseJson);
 
 
 }
