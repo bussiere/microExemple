08c1098a045868529e339edbc22cb9b1cf3bd41c
bussiere <bussiere@gmail.com>
2016-11-05 22:41:22 +0100

    debut

---
+++ login/README.md
@@ -0,0 +1,8 @@
+## MATCHER
+Matching REST service for Adserv
+
+
+### REST 
+
+    GET /v1/match/:websiteID
+
---
+++ planningUser/v1/login.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+function testLogin(req,res,callback) {
+    var wid     = req.params.wid;
+    res.status(200).json("OK");
+
+
+}
+
+
+exports.testLogin   = testLogin;
